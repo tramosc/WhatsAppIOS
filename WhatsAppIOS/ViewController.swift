86579605038e9a2435954ab4a24e9bced9a5d8d2
MAC06 <toni.ramos@tecsup.edu.pe>
2019-05-29 14:43:11 -0500

    Initial Commit

---
+++ WhatsAppIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatsAppIOS
+//
+//  Created by MAC06 on 29/05/19.
+//  Copyright © 2019 Toni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
